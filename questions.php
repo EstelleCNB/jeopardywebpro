3356673c3c151612a488ba79d5da30189e938ffa
aaliyah fievre <aaliyahfievre@gmail.com>
2026-04-17 11:44:40 -0400

    added questions.php; updated require statements in clues.php and jep.php

---
+++ questions.php
@@ -0,0 +1,132 @@
+<?php
+
+$categories = [
+    'Science',
+    'History',
+    'Geography',
+    'Literature',
+    'Pop Culture',
+];
+
+$clues = [
+    1 => [
+        'category' => 'Science',
+        'value' => 200,
+        'answer' => 'This force pulls objects toward Earth.',
+        'question' => 'gravity',
+    ],
+    2 => [
+        'category' => 'Science',
+        'value' => 400,
+        'answer' => 'H2O is the chemical formula for this.',
+        'question' => 'water',
+    ],
+    3 => [
+        'category' => 'Science',
+        'value' => 600,
+        'answer' => 'This planet is known as the Red Planet.',
+        'question' => 'mars',
+    ],
+    4 => [
+        'category' => 'Science',
+        'value' => 800,
+        'answer' => 'The center of an atom is called this.',
+        'question' => 'nucleus',
+    ],
+    5 => [
+        'category' => 'History',
+        'value' => 200,
+        'answer' => 'This ancient civilization built the pyramids at Giza.',
+        'question' => 'the egyptians',
+    ],
+    6 => [
+        'category' => 'History',
+        'value' => 400,
+        'answer' => 'He was the first President of the United States.',
+        'question' => 'george washington',
+    ],
+    7 => [
+        'category' => 'History',
+        'value' => 600,
+        'answer' => 'This wall fell in 1989, marking the end of the Cold War era.',
+        'question' => 'berlin wall',
+    ],
+    8 => [
+        'category' => 'History',
+        'value' => 800,
+        'answer' => 'The ship that sank on its maiden voyage in 1912.',
+        'question' => 'titanic',
+    ],
+    9 => [
+        'category' => 'Geography',
+        'value' => 200,
+        'answer' => 'This is the largest ocean on Earth.',
+        'question' => 'pacific ocean',
+    ],
+    10 => [
+        'category' => 'Geography',
+        'value' => 400,
+        'answer' => 'This country is home to the city of Kyoto.',
+        'question' => 'japan',
+    ],
+    11 => [
+        'category' => 'Geography',
+        'value' => 600,
+        'answer' => 'Mount Everest lies in this mountain range.',
+        'question' => 'himalayas',
+    ],
+    12 => [
+        'category' => 'Geography',
+        'value' => 800,
+        'answer' => 'This river runs through Egypt and empties into the Mediterranean Sea.',
+        'question' => 'nile',
+    ],
+    13 => [
+        'category' => 'Literature',
+        'value' => 200,
+        'answer' => 'Shakespeare wrote this tragedy about two star-crossed lovers.',
+        'question' => 'romeo and juliet',
+    ],
+    14 => [
+        'category' => 'Literature',
+        'value' => 400,
+        'answer' => 'George Orwell wrote this dystopian novel in 1949.',
+        'question' => '1984',
+    ],
+    15 => [
+        'category' => 'Literature',
+        'value' => 600,
+        'answer' => 'This author created the detective Sherlock Holmes.',
+        'question' => 'arthur conan doyle',
+    ],
+    16 => [
+        'category' => 'Literature',
+        'value' => 800,
+        'answer' => 'This epic poem begins with "Sing, goddess, the anger of Achilles."',
+        'question' => 'the iliad',
+    ],
+    17 => [
+        'category' => 'Pop Culture',
+        'value' => 200,
+        'answer' => 'This singer is known as the "Queen of Pop."',
+        'question' => 'madonna',
+    ],
+    18 => [
+        'category' => 'Pop Culture',
+        'value' => 400,
+        'answer' => 'The wizarding school attended by Harry Potter.',
+        'question' => 'hogwarts',
+    ],
+    19 => [
+        'category' => 'Pop Culture',
+        'value' => 600,
+        'answer' => 'This long-running animated series features Homer and Marge Simpson.',
+        'question' => 'the simpsons',
+    ],
+    20 => [
+        'category' => 'Pop Culture',
+        'value' => 800,
+        'answer' => 'The cinematic universe that includes Iron Man and Black Panther.',
+        'question' => 'marvel cinematic universe',
+    ],
+];
